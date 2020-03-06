6b75f27ef0cc3009360b54c9f795fd734862bf80
王橙 <17610615128@yeah.net>
2020-03-06 13:25:05 +0800

    第一次提交

---
+++ src/test/java/com/leetcode/code/LeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
